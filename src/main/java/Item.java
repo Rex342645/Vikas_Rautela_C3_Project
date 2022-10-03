021afcc1fe87e4eee40faec8053ada1592447000
Rex342645 <vikasrautela88@gmail.com>
2022-10-03 16:16:18 +0530

    Part 2: Solution

---
+++ src/main/java/Item.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class Item {
+    private String name;
+    private int price;
+
+    public Item(String name)
+    {
+        this.name=name;
+    }
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice(){return price;}
+    @Override
+    public String toString(){
+        return  name + ":"
+                + price
+                + "\n"
+                ;
+    }
+}
